744d6f5b057581299b1a91c2ee8f9aad9429ea18
lucassp12 <lucas_santos_pereira@hotmail.com>
2019-05-22 17:40:34 -0300

    Criação de Model e Controller de User

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+module.exports = {
+  secret: "revenda",
+  ttl: 86400
+};
--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,7 @@ const express = require("express");
 
 const routes = express.Router();
 
-routes.get("/", function(req, res) {
-  res.send("hellow world");
-});
+const UserController = require("./app/controllers/UserController");
+routes.post("/user", UserController.store);
 
 module.exports = routes;
